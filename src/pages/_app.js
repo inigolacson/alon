f81f1a4c82faa81e4cfdba939e3430ff89d182b8
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-05 20:10:22 +0800

    init commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Component {...pageProps} />;
+    </main>
+  )
 }
