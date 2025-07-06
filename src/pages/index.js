3ccb870f21c14683c072415d7ed741dfa9fd7610
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-06 16:58:36 +0800

    added photos and showed 1 pic inside hero container

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,9 @@ export default function Home() {
               </div>
           </header>
           <section className={hero.container}>
-
+            <div className={hero.bgImage}>
+            <div className={hero.overlay}></div>
+            </div>
           </section>
         </main>
     </>
