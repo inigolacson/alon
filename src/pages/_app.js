779a74118a345f73faf7282a5c0d95b2e6dd87b4
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-06 14:50:25 +0800

    saved _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return (
-    <main>
+    <main>  
       <Component {...pageProps} />;
     </main>
   )
