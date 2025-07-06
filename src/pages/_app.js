0dd4baff4de80591dc1bc22bcc8bebf38ae964c6
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-06 14:36:27 +0800

    Setup fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,8 @@
 import "@/styles/globals.scss";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${geistSans.variable} ${geistMono.variable}`}>
+    <main>
       <Component {...pageProps} />;
     </main>
   )
