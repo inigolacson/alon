09a9cf4a79cc58da2bfb05da6c4581b967733234
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-07 16:14:44 +0800

    added heroInfo

---
+++ src/data/hero-info.js
@@ -0,0 +1,33 @@
+
+export const heroInfo = [
+    {
+        image: '/photos/hero/CT20S-HERO.webp',
+        heading: 'CT20S',
+        subtext: '',
+    },
+    {
+        image: '/photos/hero/T26-HERO.webp',
+        heading: 'T26',
+        subtext: '',
+    },
+    {
+        image: '/photos/hero/GT20-HERO.webp',
+        heading: 'GT20',
+        subtext: '',
+    },
+    {
+        image: '/photos/hero/CT20-HERO.webp',
+        heading: 'CT20',
+        subtext: '',
+    },
+    {
+        image: '/photos/hero/M20-HERO.webp',
+        heading: 'M20',
+        subtext: '',
+    },
+    {
+        image: '/photos/hero/T20-HERO.webp',
+        heading: 'T20',
+        subtext: '',
+    },
+]
